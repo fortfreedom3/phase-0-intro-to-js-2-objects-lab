51969c6a74d97d39094f54b525428dff5c6a39dc
Morgan <morganlbyrne@gmail.com>
2021-11-05 11:21:15 -0400

    commit tree 49d47da46a9112a976803d31f22741eb950c4ff3

--- index.js
+++ index.js
@@ -4,21 +4,55 @@ const employee = {
 }
 
 function updateEmployeeWithKeyAndValue(obj, key, value) {
-    return Object.assign({},obj,{[key]:value});
-};
-
-function destructivelyUpdateEmployeeWithKeyAndValue(obj,key,value){
-    obj[key]=value;
-    return employee;
+    let copy = {...obj};
+    copy[key] = value;
+    return copy;
 }
 
-function deleteFromEmployeeByKey(obj, key){
-    const newEmployee = {...obj, ...key};
-    delete newEmployee[key]
+// updateEmployeeWithKeyAndValue(employee, 'streetAddress', '11 Broadway')
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value;
+    return obj;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = {...employee};
+    delete newEmployee[key];
     return newEmployee;
 }
 
-function destructivelyDeleteFromEmployeeByKey(obj, key) {
-    delete obj[key];
-    return obj;
-}
+const destructivelyDeleteFromEmployeeByKey = (employee, key) => {
+    delete employee[key];  
+    return employee;  
+};
+
+// console.log(deleteFromEmployeeByKey(employee, 'name'));
+
+
+
+
+// const employee = {
+//     name: "Meri Pippin", 
+//     streetAddress: "Route 637, El Camino, Spain",
+// }
+
+// function updateEmployeeWithKeyAndValue(obj, key, value) {
+//     return Object.assign({},obj,{[key]:value});
+// };
+
+// function destructivelyUpdateEmployeeWithKeyAndValue(obj,key,value){
+//     obj[key]=value;
+//     return employee;
+// }
+
+// function deleteFromEmployeeByKey(obj, key){
+//     const newEmployee = {...obj, ...key};
+//     delete newEmployee[key]
+//     return newEmployee;
+// }
+
+// function destructivelyDeleteFromEmployeeByKey(obj, key) {
+//     delete obj[key];
+//     return obj;
+// }
